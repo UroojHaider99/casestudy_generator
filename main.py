a3dbfa9da6df2ecbd09effb25e10bb34f2916ca8
Urooj Haider <132275621+UroojHaider99@users.noreply.github.com>
2023-05-02 06:35:29 +0500

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ popular_roles = ["Software Developer", "Data Scientist", "Product Manager", "Sal
                  "Customer Support Representative", "Business Analyst", "Graphic Designer", "Content Writer", "UI/UX Designer"]
 # Define the role selection
 #st.markdown("<h4 style='font-family: Bebas Neue;'>Select Role Name</h4>", unsafe_allow_html=True)
-role_option = st.selectbox("<h3>Select Role Name</h3>",popular_roles, key="role_option")
+role_option = st.selectbox("Select Role Name",popular_roles, key="role_option")
 
 if role_option == "Other":
     role_input = st.text_input(label="Enter custom role name:", key="role_input")
